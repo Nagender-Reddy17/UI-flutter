4328a008e793801db7f3d414ed6a3f1d1143ff55
Nagender-Reddy17 <nagenderreddy1707@gmail.com>
2025-03-05 11:31:23 +0530

    Add files via upload

---
+++ hello.dart
@@ -0,0 +1,18 @@
+ int sum(int x,int y){
+    return x+y;
+}
+int mul(int x, int y){
+    return x*y;
+}
+double div(double x,double y){
+   	return x/y;
+}double muld(double a, double b){
+    return (a*b);
+}
+
+ void main(){
+print(sum(2,5));
+print(mul(4,3));
+print(div(10,2));
+print(muld(5,2));
+}
