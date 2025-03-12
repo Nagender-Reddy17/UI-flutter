37e50511a05285b481433a15c1440175893973fe
Nagender-Reddy17 <nagenderreddy1707@gmail.com>
2025-03-12 11:14:27 +0530

    Add files via upload

---
+++ switch_demo/bin/switch_demo.dart
@@ -0,0 +1,6 @@
+import 'package:switch_demo/switch_demo.dart' as switch_demo;
+
+void main(List<String> arguments) {
+  print('Hello world:!');
+print('${ switch_demo.switchd1('red')}');
+}
---
+++ switch_demo/lib/switch_demo.dart
@@ -0,0 +1,9 @@
+String switchd1(String color){
+    return  switch(color){
+               'red' || 'blue' => 'blue or red',
+		'black' => 'black',
+		'green' => 'green',
+		_=> 'no color',
+		
+};
+}
